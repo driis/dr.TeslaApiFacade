3212613a4f31b05117236b1a0c3a9e2f1d4970eb
Dennis Riis <dr@driis.dk>
2021-07-29 21:00:29 +0200

    Add skeleton WebApi project.

---
+++ dr.TeslaApiFacade.Tests/ApiSanityTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace dr.TeslaApiFacade.Tests
+{
+    [TestFixture]
+    public class ApiSanityTests
+    {
+        private WebApplicationFactory<Startup>? _appFactory;
+
+        private HttpClient Client => _appFactory!.CreateClient();
+        
+        [OneTimeSetUp]
+        public void BeforeAll()
+        {
+            _appFactory = new WebApplicationFactory<Startup>();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAll()
+        {
+            _appFactory?.Dispose();
+        }
+
+        [Test]
+        public async Task CanStartupAndInvokeSimpleApiEndpoint()
+        {
+            var response= await Client.GetAsync("api/ping");
+            
+            Assert.That(response.IsSuccessStatusCode);
+        }
+    }
+}
---
+++ dr.TeslaApiFacade/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dr.TeslaApiFacade.Controllers
+{
+    [ApiController, Route("api/ping")]
+    public class PingController
+    {
+        public object Get()
+        {
+            return new { message = "Pong" };
+        }
+    }
+}
